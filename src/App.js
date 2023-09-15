0c0da6c40b12051cdccedbeebd38404b0a608145
kalimuthu <kalimuthupethappan@gmail.com>
2023-09-15 16:49:32 +0530

    first day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const initialItems = [
+  {
+    id: 1,
+    description: "kali",
+    finished: true,
+  },
+  {
+    id: 2,
+    description: "soundarya",
+    finished: false,
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Form />
+      <TodoList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return <header>todos</header>;
+}
+
+function Form() {
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, finished: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button>\/</button>
+      <input
+        type="text"
+        placeholder="What needs to be done?"
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+    </form>
+  );
+}
+
+function TodoList() {
+  return (
+    <ul>
+      {initialItems.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <>
+      <li>
+        <span style={item.finished ? { textDecoration: "line-through" } : {}}>
+          {item.description}
+        </span>
+        <button>&times;</button>
+      </li>
+    </>
+  );
+}
+
+function Stats() {
+  return (
+    <div>
+      <p> X item left</p>
+      <button>All</button>
+      <button>Active</button>
+      <button>Completed</button>
+    </div>
+  );
+}
